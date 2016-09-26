833ae5f6a3f713ab40a519edcf61b83b7c7cfaaf
Blair Lyons <blair208@gmail.com>
2016-09-26 14:08:20 -0700

    Better random position algorithm to avoid bunching in center

--- Assets/Scripts/Custom Behaviors/RandomVector3.cs
+++ Assets/Scripts/Custom Behaviors/RandomVector3.cs
@@ -6,45 +6,31 @@ namespace BehaviorDesigner.Runtime.Tasks.Basic.Math
 	[TaskDescription("Sets a random Vector3 value")]
 	public class RandomVector3 : Action
 	{
-		[Tooltip("The minimum amount")]
-		public SharedVector3 min;
-		[Tooltip("The maximum amount")]
-		public SharedVector3 max;
-		[Tooltip("Is the maximum value inclusive?")]
-		public bool inclusive;
-		[Tooltip("Use anchor?")]
-		public SharedBool useAnchor;
-		[Tooltip("The point to anchor around")]
+		[Tooltip("The minimum location bounds")]
+		public SharedVector3 minLocation;
+		[Tooltip("The maximum location bounds")]
+		public SharedVector3 maxLocation;
+		[Tooltip("The starting position")]
 		public SharedVector3 anchor;
-		[Tooltip("The maximum distance result can be from anchor")]
-		public SharedFloat maxDistanceFromAnchor;
+		[Tooltip("The maximum amount")]
+		public SharedFloat maxDistance;
 		[Tooltip("The variable to store the result")]
 		public SharedVector3 storeResult;
 
 		public override TaskStatus OnUpdate()
 		{
-			Vector3 random = Vector3.zero;
+			Vector3 random = anchor.Value + maxDistance.Value * Random.insideUnitSphere;
 
-			if (inclusive) {
-				random = new Vector3( Random.Range( min.Value.x, max.Value.x + 1 ), Random.Range( min.Value.y, max.Value.y + 1 ), Random.Range( min.Value.z, max.Value.z + 1 ) );
-			} else {
-				random = new Vector3( Random.Range( min.Value.x, max.Value.x ), Random.Range( min.Value.y, max.Value.y ), Random.Range( min.Value.z, max.Value.z ) );
-			}
-
-			if (useAnchor.Value)
-			{
-				random = anchor.Value + Vector3.ClampMagnitude( random - anchor.Value, maxDistanceFromAnchor.Value );
-			}
-
-			storeResult.Value = random;
+			storeResult.Value = new Vector3( Mathf.Clamp( random.x, minLocation.Value.x, maxLocation.Value.x ), 
+				Mathf.Clamp( random.y, minLocation.Value.y, maxLocation.Value.y ), Mathf.Clamp( random.z, minLocation.Value.z, maxLocation.Value.z ) );
 			return TaskStatus.Success;
 		}
 
 		public override void OnReset()
 		{
-			min.Value = Vector3.zero;
-			max.Value = Vector3.zero;
-			inclusive = false;
+			minLocation.Value = -Vector3.one;
+			maxLocation.Value = Vector3.one;
+			maxDistance.Value = 1f;
 			storeResult.Value = Vector3.zero;
 		}
 	}

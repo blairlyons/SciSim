0d93e221b2796885624444a297be4fb848eb60ef
Blair Lyons <blair208@gmail.com>
2016-09-26 17:39:23 -0700

    Consolidated random motion into external behavior with minimal inputs

---
+++ Assets/Scripts/Custom Behaviors/MoveGameobjectTowards.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace BehaviorDesigner.Runtime.Tasks.Basic.UnityVector3
+{
+	[TaskCategory("SciSim/Animation")]
+	[TaskDescription("Move the gameobject from the current position to the target position.")]
+	public class MoveGameobjectTowards : Action
+	{
+		[Tooltip("The gameobject to move")]
+		public SharedGameObject gameobjectToMove;
+		[Tooltip("The target position")]
+		public SharedVector3 targetPosition;
+		[Tooltip("The movement speed")]
+		public SharedFloat speed;
+
+		public override TaskStatus OnUpdate()
+		{
+			gameobjectToMove.Value.transform.position = Vector3.MoveTowards(gameobjectToMove.Value.transform.position, targetPosition.Value, speed.Value * Time.deltaTime);
+			return TaskStatus.Success;
+		}
+
+		public override void OnReset()
+		{
+			targetPosition = Vector3.zero;
+			speed = 0;
+		}
+	}
+}
--- Assets/Scripts/Custom Behaviors/SetRandomVector3.cs
+++ Assets/Scripts/Custom Behaviors/SetRandomVector3.cs
@@ -6,20 +6,20 @@ namespace BehaviorDesigner.Runtime.Tasks.Basic.Math
 	[TaskDescription("Sets a random Vector3 value")]
 	public class SetRandomVector3 : Action
 	{
-		[Tooltip("The minimum location bounds")]
+		[Tooltip("The minimum location")]
 		public SharedVector3 minLocation;
-		[Tooltip("The maximum location bounds")]
+		[Tooltip("The maximum location")]
 		public SharedVector3 maxLocation;
-		[Tooltip("The starting position")]
-		public SharedVector3 anchor;
-		[Tooltip("The maximum amount")]
+		[Tooltip("The gameobject to use for starting position")]
+		public SharedGameObject anchorGameobject;
+		[Tooltip("The maximum distance between anchor and result")]
 		public SharedFloat maxDistance;
 		[Tooltip("The variable to store the result")]
 		public SharedVector3 storeResult;
 
 		public override TaskStatus OnUpdate()
 		{
-			Vector3 random = anchor.Value + maxDistance.Value * Random.insideUnitSphere;
+			Vector3 random = anchorGameobject.Value.transform.position + maxDistance.Value * Random.insideUnitSphere;
 
 			storeResult.Value = new Vector3( Mathf.Clamp( random.x, minLocation.Value.x, maxLocation.Value.x ), 
 				Mathf.Clamp( random.y, minLocation.Value.y, maxLocation.Value.y ), Mathf.Clamp( random.z, minLocation.Value.z, maxLocation.Value.z ) );

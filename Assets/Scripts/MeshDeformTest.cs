4a1dc74d8df0d8caf926ce53f27ee89027d49f4a
Blair Lyons <blair208@gmail.com>
2016-12-13 15:56:30 -0800

    added mesh deform test scene, removed GoogleVR for now

---
+++ Assets/Scripts/MeshDeformTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeshDeformTest : MonoBehaviour 
+{
+	void Update() 
+	{
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		Vector3[] vertices = mesh.vertices;
+		Vector3[] normals = mesh.normals;
+		int i = 0;
+		while (i < vertices.Length) 
+		{
+			vertices[i] += normals[i] * 0.01f * Mathf.Sin(Time.time);
+			i++;
+		}
+		mesh.vertices = vertices;
+	}
+}
